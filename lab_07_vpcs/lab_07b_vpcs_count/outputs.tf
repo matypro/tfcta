aa15c5198fe9b2c0a64e7a04f2b85b23d70573bc
matypro <serherrero@gmail.com>
2023-03-22 17:00:59 +0100

    jueves

--- lab_07_vpcs/lab_07b_vpcs_count/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "publics1" {
-   description =  "red publica1"
-   value       = data.aws_subnets.publics.ids.0
- }
-
-output "publics2" {
-   description =  "red publica2"
-   value       = data.aws_subnets.publics.ids.1
- }
--- lab_07_vpcs/lab_07b_vpcs_count/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-lab_number   = "07a"
-special_port = 666
-vpc-id = "vpc-002f3c10529608a7a"
